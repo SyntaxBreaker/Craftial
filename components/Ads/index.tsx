db6de6fa96967c2126738f15e7401a54597c018d
xMetaneRx <60369185+xMetaneRx@users.noreply.github.com>
2022-10-18 16:13:18 +0200

    Add ads container

---
+++ components/Ads/index.tsx
@@ -0,0 +1,13 @@
+import styles from "./ads.module.scss";
+
+function Ads() {
+    return (
+        <section className={styles.wrapper}>
+            <div className={styles.ads}>
+                <p>ADS</p>
+            </div>
+        </section>
+    );
+}
+
+export default Ads;
