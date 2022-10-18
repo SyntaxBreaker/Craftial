a269889f7c04050325e3bd9d2005f95bd8cc0afa
xMetaneRx <60369185+xMetaneRx@users.noreply.github.com>
2022-10-18 16:22:57 +0200

    Add display of newly added components and global background-color

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import type { NextPage } from 'next'
+import Ads from '../components/Ads'
+import Categories from '../components/Categories'
 import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
     <>
       <Header />
+      <Ads />
+      <Categories />
     </>
   )
 }
