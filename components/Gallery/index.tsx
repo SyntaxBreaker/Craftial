0605a04d2f5f07122325babfa7dc3ea80ed511a7
xMetaneRx <60369185+xMetaneRx@users.noreply.github.com>
2022-10-22 16:22:10 +0200

    Add mobile layout support for the offer page

--- components/Gallery/index.tsx
+++ components/Gallery/index.tsx
@@ -1,60 +1,106 @@
 import styles from "./gallery.module.scss";
-import { useState } from "react";
+import { useState, useEffect, Fragment } from "react";
 import Image from "next/image";
-import ArrowLeft from '../../public/arrow-left.svg';
-import ArrowRight from '../../public/arrow-right.svg'
+import ArrowLeft from "../../public/arrow-left.svg";
+import ArrowRight from "../../public/arrow-right.svg";
 
-const images: string[] = [
-    "https://cdn.pixabay.com/photo/2022/10/16/13/53/early-morning-7525151_960_720.jpg",
-    "https://cdn.pixabay.com/photo/2022/08/26/04/19/beach-7411683_960_720.jpg",
-    "https://cdn.pixabay.com/photo/2022/10/13/07/54/crane-houses-7518536_960_720.jpg",
+interface IImage {
+    id: number;
+    url: string;
+}
+
+const images: IImage[] = [
+    {
+        id: 0,
+        url: 'https://cdn.pixabay.com/photo/2022/10/16/13/53/early-morning-7525151_960_720.jpg'
+    },
+    {
+        id: 1,
+        url: 'https://cdn.pixabay.com/photo/2022/08/26/04/19/beach-7411683_960_720.jpg'
+    },
+    {
+        id: 2,
+        url: 'https://cdn.pixabay.com/photo/2022/10/13/07/54/crane-houses-7518536_960_720.jpg'
+    }
 ];
 
 function Gallery() {
     const [imageIndex, setImageIndex] = useState<number>(0);
+    const [isMobile, setIsMobile] = useState<boolean>(false);
+
+    useEffect(() => {
+        if (window.innerWidth > 720) {
+            setIsMobile(false);
+        } else {
+            setIsMobile(true);
+        }
+    }, []);
 
     return (
-        <div className={styles['image-wrapper']}>
-            <Image
-                src={images[imageIndex]}
-                alt=""
-                width="100%"
-                height="50px"
-                sizes="(min-width: 768px) 960px, 720px"
-                className={styles["gallery__image"]}
-            />
-            <button
-                className={`${styles['gallery__button']} ${styles['gallery__button--prev']}`}
-                onClick={() =>
-                    setImageIndex(
-                        imageIndex === 0 ? images.length - 1 : imageIndex - 1
-                    )
-                }
-            >
-                <Image
-                src={'/arrow-left.svg'}
-                alt=""
-                width="100%"
-                height="50px"
-                sizes="(min-width: 768px) 960px, 720px"
-            />
-            </button>
-            <button
-                className={`${styles['gallery__button']} ${styles['gallery__button--next']}`}
-                onClick={() =>
-                    setImageIndex(
-                        imageIndex === images.length - 1 ? 0 : imageIndex + 1
-                    )
-                }
-            >
-                <Image
-                src={'/arrow-right.svg'}
-                alt=""
-                width="100%"
-                height="50px"
-                sizes="(min-width: 768px) 960px, 720px"
-            />
-            </button>
+        <div className={styles["gallery-wrapper"]}>
+            {isMobile ? (
+                <>
+                    <Image
+                        src={images.find(image => imageIndex === image.id)?.url}
+                        alt=""
+                        width="150px"
+                        height="150px"
+                        sizes="(min-width: 768px) 960px, 720px"
+                        className={styles["gallery__image"]}
+                    />
+                    <button
+                        className={`${styles["gallery__button"]} ${styles["gallery__button--prev"]}`}
+                        onClick={() =>
+                            setImageIndex(
+                                imageIndex === 0
+                                    ? images.length - 1
+                                    : imageIndex - 1
+                            )
+                        }
+                    >
+                        <Image
+                            src={"/arrow-left.svg"}
+                            alt=""
+                            width="100%"
+                            height="50px"
+                            sizes="(min-width: 768px) 960px, 720px"
+                        />
+                    </button>
+                    <button
+                        className={`${styles["gallery__button"]} ${styles["gallery__button--next"]}`}
+                        onClick={() =>
+                            setImageIndex(
+                                imageIndex === images.length - 1
+                                    ? 0
+                                    : imageIndex + 1
+                            )
+                        }
+                    >
+                        <Image
+                            src={"/arrow-right.svg"}
+                            alt=""
+                            width="100%"
+                            height="50px"
+                            sizes="(min-width: 768px) 960px, 720px"
+                        />
+                    </button>
+                </>
+            ) : (
+                <>
+                    {images.map((image, index) => (
+                        <Fragment key={image.id}>
+                            <Image
+                                src={image.url}
+                                alt=""
+                                width="150px"
+                                height="150px"
+                                sizes="(min-width: 768px) 960px, 720px"
+                                className={styles["gallery__image"]}
+                            />
+                        </Fragment>
+                    ))}
+                </>
+            )}
         </div>
     );
 }
--- components/OfferDetails/index.tsx
+++ components/OfferDetails/index.tsx
@@ -1,9 +1,18 @@
+import styles from './offerDetails.module.scss';
+
 function OfferDetails() {
     return (
-       <>
-        <h2>Offer Details</h2>
-       </> 
-    )
+        <div className={styles['offer-details']}>
+            <h2 className={styles['offer-details__title']}>Offer Details</h2>
+            <p className={styles['offer-details__price']}>Price: <span className={styles['offer-details__price--bold']}>$10.000</span></p>
+            <p className={styles['offer-details__description']}>
+                Description: <span className={styles['offer-details__description--bold']}>Lorem ipsum dolor, sit amet consectetur adipisicing
+                elit. Laborum facere aut maiores accusamus explicabo eveniet,
+                asperiores fugit autem recusandae voluptates, rem tenetur, neque
+                laudantium iusto atque nihil! Fugit, explicabo nostrum? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur adipisci dolorum ducimus laboriosam error! Inventore, labore. Atque mollitia, eveniet sunt vitae earum esse minus facere, dicta similique, quam vero explicabo. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste, ea a laborum maxime odio excepturi. Sit aliquid, pariatur est, numquam, quaerat ad aspernatur excepturi dolor quisquam vero vel! Placeat, maxime.</span>
+            </p>
+        </div>
+    );
 }
 
 export default OfferDetails;
