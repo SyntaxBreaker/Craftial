c15e835b246e768e90fbe1409012913dfbf2b9ba
xMetaneRx <60369185+xMetaneRx@users.noreply.github.com>
2022-10-23 16:29:43 +0200

    Change OfferDetails layout

--- components/OfferDetails/index.tsx
+++ components/OfferDetails/index.tsx
@@ -3,10 +3,10 @@ import styles from './offerDetails.module.scss';
 function OfferDetails() {
     return (
         <div className={styles['offer-details']}>
-            <h2 className={styles['offer-details__title']}>Offer Details</h2>
-            <p className={styles['offer-details__price']}>Price: <span className={styles['offer-details__price--bold']}>$10.000</span></p>
+            <h2 className={styles['offer-details__title']}>Lorem ipsum</h2>
+            <p className={styles['offer-details__price']}>$10.000</p>
             <p className={styles['offer-details__description']}>
-                Description: <span className={styles['offer-details__description--bold']}>Lorem ipsum dolor, sit amet consectetur adipisicing
+                Description: <span className={`${styles['offer-details__description--bold']} ${styles['offer-details__description--green']}`}>Lorem ipsum dolor, sit amet consectetur adipisicing
                 elit. Laborum facere aut maiores accusamus explicabo eveniet,
                 asperiores fugit autem recusandae voluptates, rem tenetur, neque
                 laudantium iusto atque nihil! Fugit, explicabo nostrum? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur adipisci dolorum ducimus laboriosam error! Inventore, labore. Atque mollitia, eveniet sunt vitae earum esse minus facere, dicta similique, quam vero explicabo. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste, ea a laborum maxime odio excepturi. Sit aliquid, pariatur est, numquam, quaerat ad aspernatur excepturi dolor quisquam vero vel! Placeat, maxime.</span>
