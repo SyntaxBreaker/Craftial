acc72f8c5955f99fe26818266ab9a407f74492aa
xMetaneRx <60369185+xMetaneRx@users.noreply.github.com>
2022-10-24 01:09:58 +0200

    Add location, phone number and email

--- components/OfferDetails/index.tsx
+++ components/OfferDetails/index.tsx
@@ -1,10 +1,16 @@
 import styles from './offerDetails.module.scss';
+import Image from 'next/image';
 
 function OfferDetails() {
     return (
         <div className={styles['offer-details']}>
             <h2 className={styles['offer-details__title']}>Lorem ipsum</h2>
-            <p className={styles['offer-details__price']}>$10.000</p>
+            <p className={`${styles['offer-details__location']} ${styles['offer-details__location--bold']}`}>Sandwell, West Midlands</p>
+            <p className={`${styles['offer-details__price']} ${styles['offer-details__price--bold']}`}>$10.000</p>
+            <div className={styles['offer-contact']}>
+                    <p className={`${styles['offer-contact__email']} ${styles['offer-contact__email--bold']}`}>test@example.com</p>
+                    <p className={`${styles['offer-contact__phone-number']} ${styles['offer-contact__phone-number--bold']}`}>+48 123 456 789</p>
+            </div>
             <p className={styles['offer-details__description']}>
                 Description: <span className={`${styles['offer-details__description--bold']} ${styles['offer-details__description--green']}`}>Lorem ipsum dolor, sit amet consectetur adipisicing
                 elit. Laborum facere aut maiores accusamus explicabo eveniet,
