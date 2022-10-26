4697ec6cab37e5867a251b4be0ed5dc93a4b727c
xMetaneRx <60369185+xMetaneRx@users.noreply.github.com>
2022-10-27 00:12:35 +0200

    Apply SASS variables

--- types.ts
+++ types.ts
@@ -10,5 +10,5 @@ export interface IOffer {
 }
 
 export interface IOffers {
-    offers: IOffer[]
+    offers: IOffer[];
 }
--- utils/mongoDB.ts
+++ utils/mongoDB.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
-const connectToMongoDB = async () => mongoose.connect(process.env.MONGODB_URI as string);
+const connectToMongoDB = async () =>
+    mongoose.connect(process.env.MONGODB_URI as string);
 
 export default connectToMongoDB;
