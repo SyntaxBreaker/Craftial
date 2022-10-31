55bdbc0ce2f22d48cdc13acfac5b7b9840230213
xMetaneRx <60369185+xMetaneRx@users.noreply.github.com>
2022-11-01 00:55:02 +0100

    Add loading to offer layout

--- components/OfferLayout/index.tsx
+++ components/OfferLayout/index.tsx
@@ -1,13 +1,30 @@
+import { useState, useEffect } from "react";
 import Gallery from "../Gallery";
 import OfferDetails from "../OfferDetails";
 import styles from "./offerLayout.module.scss";
 import { IOffer } from "../../types";
 
 function OfferLayout(offer: IOffer) {
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        Object.keys(offer).length === 0
+            ? setIsLoading(true)
+            : setIsLoading(false);
+    }, [offer]);
+
     return (
         <section className={styles["offer-layout"]}>
-            <Gallery />
-            <OfferDetails {...offer} />
+            {isLoading ? (
+                <div className={styles["offer-layout__loading"]}>
+                    Please be patient, loading...
+                </div>
+            ) : (
+                <>
+                    <Gallery />
+                    <OfferDetails {...offer} />
+                </>
+            )}
         </section>
     );
 }
