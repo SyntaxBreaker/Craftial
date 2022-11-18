0a67445d6172dbc4e325d94a03658618a0a2c5e9
xMetaneRx <60369185+xMetaneRx@users.noreply.github.com>
2022-11-19 00:46:01 +0100

    Add a lang attribute

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
   images: {
     domains: ['cdn.pixabay.com', 'i.ibb.co']
   },
