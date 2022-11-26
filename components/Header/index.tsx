b74bf879ca3623d528d57f20fe6a6f95aa93bb6b
xMetaneRx <60369185+xMetaneRx@users.noreply.github.com>
2022-11-26 14:30:41 +0100

    Change icons in the header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,6 +1,7 @@
 import styles from "./header.module.scss";
 import Link from "next/link";
 import { useUser } from "@auth0/nextjs-auth0";
+import Image from "next/image";
 
 function Header() {
     const { user, error, isLoading } = useUser();
@@ -15,23 +16,35 @@ function Header() {
             <div className={styles["account"]}>
                 <span className={styles["account__item"]}>
                     <Link href="/newOffer">
-                        <a className={styles["account__link"]}>➕</a>
+                        <a className={styles["account__link"]}>
+                            <Image src="/plus.svg" width="32px" height="32px" alt="Create a new offer" />
+                        </a>
                     </Link>
                 </span>
                 <span className={styles["account__item"]}>
                     <Link href="/favorites">
-                        <a className={styles["account__link"]}>💙</a>
+                        <a className={styles["account__link"]}>
+                            <Image src="/heart.svg" width="24px" height="24px" alt="Redirect to favorite offers" />
+                        </a>
                     </Link>
                 </span>
                 {user ? (
                     <>
                         <Link href="/api/auth/logout">
-                            <a className={`${styles["account__link"]} ${styles["account__link--white"]}`}>Logout</a>
+                            <a
+                                className={`${styles["account__link"]} ${styles["account__link--white"]}`}
+                            >
+                                Logout
+                            </a>
                         </Link>
                     </>
                 ) : (
                     <Link href="/api/auth/login">
-                        <a className={`${styles["account__link"]} ${styles["account__link--white"]}`}>Login</a>
+                        <a
+                            className={`${styles["account__link"]} ${styles["account__link--white"]}`}
+                        >
+                            Login
+                        </a>
                     </Link>
                 )}
             </div>
