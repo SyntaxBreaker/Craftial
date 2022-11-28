d83644f33d58304bc778da79a76bf3a82f90a124
xMetaneRx <60369185+xMetaneRx@users.noreply.github.com>
2022-11-28 14:56:27 +0100

    Improve the appearance of the buttons in the header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -4,52 +4,62 @@ import { useUser } from "@auth0/nextjs-auth0";
 import Image from "next/image";
 
 function Header() {
-    const { user, error, isLoading } = useUser();
+  const { user, error, isLoading } = useUser();
 
-    return (
-        <header className={styles["header"]}>
-            <h1 className={styles["header__logo"]}>
-                <Link href="/">
-                    <a className={styles["header__link"]}>Craftial</a>
-                </Link>
-            </h1>
-            <div className={styles["account"]}>
-                <span className={styles["account__item"]}>
-                    <Link href="/newOffer">
-                        <a className={styles["account__link"]}>
-                            <Image src="/plus.svg" width="32px" height="32px" alt="Create a new offer" />
-                        </a>
-                    </Link>
-                </span>
-                <span className={styles["account__item"]}>
-                    <Link href="/favorites">
-                        <a className={styles["account__link"]}>
-                            <Image src="/heart.svg" width="24px" height="24px" alt="Redirect to favorite offers" />
-                        </a>
-                    </Link>
-                </span>
-                {user ? (
-                    <>
-                        <Link href="/api/auth/logout">
-                            <a
-                                className={`${styles["account__link"]} ${styles["account__link--white"]}`}
-                            >
-                                Logout
-                            </a>
-                        </Link>
-                    </>
-                ) : (
-                    <Link href="/api/auth/login">
-                        <a
-                            className={`${styles["account__link"]} ${styles["account__link--white"]}`}
-                        >
-                            Login
-                        </a>
-                    </Link>
-                )}
-            </div>
-        </header>
-    );
+  return (
+    <header className={styles["header"]}>
+      <h1 className={styles["header__logo"]}>
+        <Link href="/">
+          <a className={styles["header__link"]}>Craftial</a>
+        </Link>
+      </h1>
+      <div className={styles["account"]}>
+        <span className={styles["account__item"]}>
+          <Link href="/newOffer">
+            <a className={styles["account__link"]}>
+              <Image
+                src="/plus.svg"
+                width="32px"
+                height="32px"
+                alt="Create a new offer"
+              />
+            </a>
+          </Link>
+        </span>
+        <span className={styles["account__item"]}>
+          <Link href="/favorites">
+            <a className={styles["account__link"]}>
+              <Image
+                src="/heart.svg"
+                width="32px"
+                height="32px"
+                alt="Redirect to favorite offers"
+              />
+            </a>
+          </Link>
+        </span>
+        {user ? (
+          <>
+            <Link href="/api/auth/logout">
+              <a
+                className={`${styles["account__link"]} ${styles["account__link--white"]}`}
+              >
+                Logout
+              </a>
+            </Link>
+          </>
+        ) : (
+          <Link href="/api/auth/login">
+            <a
+              className={`${styles["account__link"]} ${styles["account__link--white"]}`}
+            >
+              Login
+            </a>
+          </Link>
+        )}
+      </div>
+    </header>
+  );
 }
 
 export default Header;
