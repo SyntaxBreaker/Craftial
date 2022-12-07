a9e566cbf59643a25166812c3f5d19dd6f55fc75
xMetaneRx <60369185+xMetaneRx@users.noreply.github.com>
2022-12-07 17:24:49 +0100

    Change the appearance of profile details

--- components/ProfileDetails/index.tsx
+++ components/ProfileDetails/index.tsx
@@ -8,7 +8,18 @@ function ProfileDetails(user: IUser) {
       <div className={styles["profile-details__image-wrapper"]}>
         <Image src={user.picture} height={100} width={100} />
       </div>
-      {user.nickname}
+      <p className={styles["profile-details__email"]}>
+        Email:{" "}
+        <span className={styles["profile-details__email--bold"]}>
+          {user.email}
+        </span>
+      </p>
+      <p className={styles["profile-details__nickname"]}>
+        Nickname:{" "}
+        <span className={styles["profile-details__nickname--bold"]}>
+          {user.nickname}
+        </span>
+      </p>
     </section>
   );
 }
