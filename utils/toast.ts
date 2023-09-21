245e1b616b6276ca2393a611ac5d1aa95b73d901
xMetaneRx <60369185+xMetaneRx@users.noreply.github.com>
2023-09-21 13:34:09 +0200

    Remove unnecessary console.log()

--- utils/toast.ts
+++ utils/toast.ts
@@ -1,8 +1,6 @@
 import { toast } from "react-toastify";
 
 export default function removeOffer(id: string, router: any) {
-  console.log(router);
-
   toast.info("The offer is being removed.", {
     position: "top-center",
   });
